27781bc33aaffb7fbc2ebd348a24259b5e93120d
larry <larry.moyo9@gmail.com>
2023-06-27 17:00:59 +0200

    corrected aws region

--- terraform/fargate-examples/core-infra/main.tf
+++ terraform/fargate-examples/core-infra/main.tf
@@ -8,7 +8,7 @@ data "aws_caller_identity" "current" {}
 
 locals {
   name   = basename(path.cwd)
-  region = "us-west-1"
+  region = "eu-west-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
