b3572b5e4de25bc57c94ef1b3bc8800ecba4f040
larry <larry.moyo9@gmail.com>
2023-07-10 12:12:41 +0200

    correct aws region, tfstate name

--- terraform/fargate-examples/backstage/versions.tf
+++ terraform/fargate-examples/backstage/versions.tf
@@ -1,15 +1,18 @@
 terraform {
-  required_version = ">= 1.1"
+  required_version = ">= 1.5.1"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.43"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0"
-    }
+  }
+  backend "s3" {
+    bucket         = "larryterraformstatebvcket"
+    key            = "terraform-backstage.tfstate"
+    profile        = "default"
+    region         = "eu-west-1"
+    encrypt        = true
+    dynamodb_table = "larryterraformstate"
   }
 }
